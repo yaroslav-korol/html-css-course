542391f9d0415ab47a6ad641df2bd8915ffb39ad
yaroslav-korol <18688588+yaroslav-korol@users.noreply.github.com>
2024-02-01 22:18:50 +0100

    Final project. Add comments and constants to js code. Final version

--- final_project/scripts/script.js
+++ final_project/scripts/script.js
@@ -1,49 +1,60 @@
+// Constants
+const COMPACT_WIDTH = '96px';
+const WIDE_WIDTH = '240px';
+const SMALL_SCREEN_WIDTH = 525;
+
 // Ensure the entire page is loaded
 document.addEventListener('DOMContentLoaded', () => {
-  // Set outer scope variables
+  // Set 'DOMContentLoaded' scope variables
   const body = document.querySelector('body');
   const menuButton = document.querySelector('.menu-icon');
   const sidebar = document.querySelector('.sidebar');
 
-  // Toggle the side navigation
+  // Menu button actions on click
   menuButton.onclick = () => {
-
-    if (window.innerWidth <= 525 && body.style.paddingLeft === '96px') {
+    // Actions for the screens with small width
+    // Hide sidebar if it's compact
+    if (window.innerWidth <= SMALL_SCREEN_WIDTH && body.style.paddingLeft === COMPACT_WIDTH) {
       sidebar.classList.replace('sidebar-compact', 'sidebar-hide');
       body.style.paddingLeft = '0';
     }
-    else if (window.innerWidth <= 525) {
+    // Switch the sidebar on compact if i'st hidden.
+    else if (window.innerWidth <= SMALL_SCREEN_WIDTH) {
       sidebar.classList.replace('sidebar-hide', 'sidebar-compact');
-      body.style.paddingLeft = '96px';
+      body.style.paddingLeft = COMPACT_WIDTH;
     }
-    else if (body.style.paddingLeft === '240px') {
-      body.style.paddingLeft = '96px';
+    // Switch the sidebar on compact if it's wide 
+    else if (body.style.paddingLeft === WIDE_WIDTH) {
+      body.style.paddingLeft = COMPACT_WIDTH;
       sidebar.classList.replace('sidebar-wide', 'sidebar-compact');
     }
+    // Switch the sidebar on wide if it's compact
     else {
-      body.style.paddingLeft = '240px';
+      body.style.paddingLeft = WIDE_WIDTH;
       sidebar.classList.replace('sidebar-compact', 'sidebar-wide');
     }
   }
 
-  // Sidebar auto resize depending on viewport with
+  // Sidebar auto resize depending on viewport width
   function toggleSidebarStyle() {
     // Get viewport width
     const width = window.innerWidth;
 
-    if (width <= 525) {
-      body.style.paddingLeft = '0'; // Reset body padding for small screens
+    // Small screen width. Hide sidebar if it's compact
+    if (width <= SMALL_SCREEN_WIDTH) {
+      body.style.paddingLeft = '0';
       sidebar.classList.replace('sidebar-compact', 'sidebar-hide');
-    } else {
-      // Check the viewport width and adjust styles accordingly
-      if (width > 1315) {
-        body.style.paddingLeft = '240px';
+    
+    // Switch the sidebar on wide if screen width is large
+    } else if (width > 1315) {
+        body.style.paddingLeft = WIDE_WIDTH;
         sidebar.classList.replace('sidebar-compact', 'sidebar-wide');
-      } else {
-        body.style.paddingLeft = '96px';
-        sidebar.classList.remove('sidebar-hide', 'sidebar-wide');
-        sidebar.classList.add('sidebar-compact');
-      }
+    
+    // Switch the sidebar on compact from hidden/wide if screen width is average
+    } else {
+      body.style.paddingLeft = COMPACT_WIDTH;
+      sidebar.classList.remove('sidebar-hide', 'sidebar-wide');
+      sidebar.classList.add('sidebar-compact');
     }
   }
 
